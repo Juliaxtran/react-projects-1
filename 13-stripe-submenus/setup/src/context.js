e2aa0fd85d50439e976985803176f47d617cea8f
Julia Tran <juliatran@Julias-MacBook-Air.local>
2022-07-05 16:52:26 -0400

    Add global context to stripe menu

--- 13-stripe-submenus/setup/src/context.js
+++ 13-stripe-submenus/setup/src/context.js
@@ -5,11 +5,34 @@ import sublinks from './data'
 
 const AppContext = React.createContext();
 
-const AppProvider= ({children}) => {
+export const AppProvider = ({ children }) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
-  const [isModalOpen, setIsModalOpen] = useState(true);
-  return
-  <AppContext.Provider
->
-  {children}
-</AppContext.Provider>}
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(true);
+  const openSidebar = () => {
+    setIsSidebarOpen(true)
+  }
+  const closeSidebar = () => {
+    setIsSidebarOpen(false)
+  }
+  const openSubmenu = () => {
+    setIsSubmenuOpen(true)
+  }
+  const closeSubmenu = () => {
+    setIsSubmenuOpen(false)
+  }
+  return <AppContext.Provider  value ={{
+    isSubmenuOpen,
+    isSidebarOpen,
+    openSubmenu,
+    openSidebar,
+    closeSubmenu,
+    closeSidebar
+  }}>
+    {children}
+  </AppContext.Provider>
+}
+
+export const useGlobalContext = () => {
+  return useContext(AppContext)
+}
+
