f4819997f9f8e9da62a4442c27b02363623e3e91
Julia Tran <juliatran@Julias-MacBook-Air.local>
2022-07-05 16:41:04 -0400

    Initial Commit

--- 13-stripe-submenus/setup/src/context.js
+++ 13-stripe-submenus/setup/src/context.js
@@ -1,2 +1,15 @@
 import React, { useState, useContext } from 'react'
 import sublinks from './data'
+
+// set global context
+
+const AppContext = React.createContext();
+
+const AppProvider= ({children}) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const [isModalOpen, setIsModalOpen] = useState(true);
+  return
+  <AppContext.Provider
+>
+  {children}
+</AppContext.Provider>}
