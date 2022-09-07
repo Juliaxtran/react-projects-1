122553ea1d624e4a3c69344a84229ce9a2e46fdc
Julia Tran <julia.kim.tran@hotmail.com>
2022-09-06 21:30:24 -0400

    Create search form

--- 15-cocktails/setup/src/components/CocktailList.js
+++ 15-cocktails/setup/src/components/CocktailList.js
@@ -9,6 +9,12 @@ const CocktailList = () => {
   if (loading) {
     return <Loading />
   }
+  // When the cocktails array is just an empty array 
+  if (cocktails.length < 1) {
+   return <h2 className='section-title'>
+      no cocktails matched your search criteria
+    </h2>
+  }
 
   return (
     <div>
